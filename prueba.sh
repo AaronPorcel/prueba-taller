c9c83bd2809263c2d84f31a90486c7dbfe287050
AaronPorcel <aaronporcel@correo.ugr.es>
2020-10-24 20:16:07 +0200

    Añadido numero elementos

--- prueba.sh
+++ prueba.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 printf "El directorio $1 contiene los siguientes archivos:\n"
 ls $1
+
+printf "El número de archivos del directorio ${1:-.} es: `ls $1 | wc -l`\n"
+
 printf "Ejecución terminada"
